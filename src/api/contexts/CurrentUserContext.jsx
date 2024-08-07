40b97e469ba62b96a8f6f196e0ec8fc7776459ce
Jamie Connell <Jamie.Connell_10@hotmail.com>
2024-08-07 20:42:50 +0000

    CurrentUserContext.jsx(add export statements)

--- src/api/contexts/CurrentUserContext.jsx
+++ src/api/contexts/CurrentUserContext.jsx
@@ -3,3 +3,22 @@ import axios from "axios";
 import { axiosReq, axiosRes } from "../../api/axiosDefaults"
 //import { useHistory } from "react-router-dom";
 import { removeTokenTimestamp, shouldRefreshToken } from "../utils/Utils";
+
+export const CurrentUserContext = createContext();
+export const SetCurrentUserContext = createContext();
+
+export const useCurrentUser = () => useContext(CurrentUserContext);
+export const useSetCurrentUser = () => useContext(SetCurrentUserContext);
+
+export const CurrentUserProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState(null);
+  //const history = useHistory();
+
+  const handleMount = async () => {
+    try {
+      const { data } = await axiosRes.get("dj-rest-auth/user/");
+      setCurrentUser(data);
+    } catch (err) {
+      // console.log(err);
+    }
+  };
