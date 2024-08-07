66acac87bbda68c8f1aec91cab833d82374dd8db
Jamie Connell <Jamie.Connell_10@hotmail.com>
2024-08-07 20:46:20 +0000

    Signin.jsx(add two more imports to file)

--- src/api/contexts/CurrentUserContext.js
+++ src/api/contexts/CurrentUserContext.js
@@ -1,6 +1,6 @@
 import React, { createContext, useContext, useEffect, useMemo, useState } from "react";
 import axios from "axios";
-import { axiosReq, axiosRes } from "../../api/axiosDefaults"
+import { axiosReq, axiosRes } from "../axiosDefaults"
 //import { useHistory } from "react-router-dom";
 import { removeTokenTimestamp, shouldRefreshToken } from "../utils/Utils";
 
