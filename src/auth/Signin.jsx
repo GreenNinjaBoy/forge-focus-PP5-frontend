b27c0c04b0ed90ac3c0f27f6f3bd38a1f0f7edd4
Jamie Connell <Jamie.Connell_10@hotmail.com>
2024-08-07 20:47:22 +0000

    Signin.jsx( Create signin function)

--- src/auth/Signin.jsx
+++ src/auth/Signin.jsx
@@ -4,3 +4,29 @@ import {Form, Button} from "react-bootstrap";
 import axios from "axios";
 import { useSetCurrentUser } from "../contexts/CurrentUserContext";
 import { setTokenTimestamp } from "../utils/Utils";
+
+function SignIn() {
+    const navigate = useNavigate();
+    const setCurrentUser = useSetCurrentUser();
+    const [signInData, setSignInData] = useState({ username: '', password: '' });
+    const [errors,setErrors] = useState({});
+
+    const handleChange = (event) => {
+        setSignInData({
+            ...signInData,
+            [event.target.name]: event.target.value,
+        });
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            const { data } = await axios.post(`/dj-rest-auth/login/`, signInData);
+            setCurrentUser(data.user);
+            setTokenTimestamp(data);
+            navigate.push('/goals');
+        } catch (err) {
+            console.log(err);
+            setErrors(err.response?.data || {});
+        }
+    };
