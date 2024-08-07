f24600657d3a83e50e364c4d3be8b36106867b84
Jamie Connell <Jamie.Connell_10@hotmail.com>
2024-08-07 20:44:03 +0000

    CurrentUserContext.jsx( UseEffect hook to file)

--- src/api/contexts/CurrentUserContext.jsx
+++ src/api/contexts/CurrentUserContext.jsx
@@ -22,3 +22,53 @@ export const CurrentUserProvider = ({ children }) => {
       // console.log(err);
     }
   };
+
+  useEffect(() => {
+    handleMount();
+  }, []);
+
+  useMemo(() => {
+    axiosReq.interceptors.request.use(
+      async (config) => {
+        if (shouldRefreshToken()) {
+          try {
+            await axios.post("/dj-rest-auth/token/refresh/");
+          } catch (err) {
+            setCurrentUser((prevCurrentUser) => {
+              if (prevCurrentUser) {
+                //history.push("/login");
+              }
+              return null;
+            });
+            removeTokenTimestamp();
+            return config;
+          }
+        }
+        return config;
+      },
+      (err) => {
+        return Promise.reject(err);
+      }
+    );
+
+    axiosRes.interceptors.response.use(
+      (response) => response,
+      async (err) => {
+        if (err.response?.status === 401) {
+          try {
+            await axios.post("/dj-rest-auth/token/refresh/");
+          } catch (err) {
+            setCurrentUser((prevCurrentUser) => {
+              if (prevCurrentUser) {
+                //history.push("/login");
+              }
+              return null;
+            });
+            removeTokenTimestamp();
+          }
+          return axios(err.config);
+        }
+        return Promise.reject(err);
+      }
+    );
+  },);
