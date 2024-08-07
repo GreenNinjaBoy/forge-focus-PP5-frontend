5fc3f13fa181850952a01cbbe1a41f69598d467f
Jamie Connell <Jamie.Connell_10@hotmail.com>
2024-08-07 21:02:15 +0000

    main.jsx( update file code to allow proper render)

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
+import React from 'react';
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { BrowserRouter as Router } from 'react-router-dom';
+import { CurrentUserProvider } from './pages/contexts/CurrentUserContext.jsx';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <Router>
+  <CurrentUserProvider>
     <App />
-  </StrictMode>,
-)
+  </CurrentUserProvider>
+  </Router>,
+  document.getElementById('root')
+);
--- src/pages/auth/Signin.jsx
+++ src/pages/auth/Signin.jsx
@@ -2,7 +2,7 @@ import React, {useState} from "react";
 import { useNavigate } from "react-router-dom";
 import {Form, Button} from "react-bootstrap";
 import axios from "axios";
-import { useSetCurrentUser } from "../contexts/CurrentUserContext";
+import { useSetCurrentUser } from "../contexts/CurrentUserContext"
 import { setTokenTimestamp } from "../utils/Utils";
 
 function SignIn() {
