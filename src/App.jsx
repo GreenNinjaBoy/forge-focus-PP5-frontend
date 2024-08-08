29996372be26c0766c85b70292009bc91cfd5bda
Jamie Connell <Jamie.Connell_10@hotmail.com>
2024-08-08 20:21:53 +0000

    Navbar.jsx( import code from bootstrap library)

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css';
 import About from './pages/About';
+import MainNavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
+      <MainNavBar />
      <About />
     </div>
   );
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,28 +3,69 @@ import { NavLink } from "react-router-dom";
 import { useCurrentUser, useSetCurrentUser } from '../pages/contexts/CurrentUserContext';
 import axios from "axios";
 import { removeTokenTimestamp } from "../pages/utils/Utils";
-import { Navbar, Nav } from "react-bootstrap";
+import { Navbar, Nav, Container, NavDropdown,Form, Button } from "react-bootstrap";
 
-const Navbar = () => {
+const MainNavbar = () => {
 
-    const currentUser = useCurrentUser();
-    const setCurrentUser = useSetCurrentUser();
+    // const currentUser = useCurrentUser();
+    // const setCurrentUser = useSetCurrentUser();
 
-    const handleSignout = async () => {
-        try {
-            await axios.post(`dj-rest-auth/logout/`);
-            removeTokenTimestamp();
-        } catch(err) {
-            consolr.log(err)
-        }
-    }
+    // const handleSignout = async () => {
+    //     try {
+    //         await axios.post(`dj-rest-auth/logout/`);
+    //         removeTokenTimestamp();
+    //     } catch(err) {
+    //         consolr.log(err)
+    //     }
+    // }
 
-    const loggedOutLinks = (
-        <>
-        <NavLink to="/Signup">SignUp</NavLink>
-        <NavLink to="/Signin">SignIn</NavLink>
-        </>
-    );
+    // const loggedOutLinks = (
+    //     <>
+    //     <NavLink to="/Signup">SignUp</NavLink>
+    //     <NavLink to="/Signin">SignIn</NavLink>
+    //     </>
+    // );
 
-    
+    return (
+        <Navbar expand="lg" className="bg-body-tertiary">
+          <Container fluid>
+            <Navbar.Brand href="#">Forge Focus</Navbar.Brand>
+            <Navbar.Toggle aria-controls="navbarScroll" />
+            <Navbar.Collapse id="navbarScroll">
+              <Nav
+                className="me-auto my-2 my-lg-0"
+                style={{ maxHeight: '100px' }}
+                navbarScroll
+              >
+                <Nav.Link href="#action1">Home</Nav.Link>
+                <Nav.Link href="#action2">Link</Nav.Link>
+                <NavDropdown title="Link" id="navbarScrollingDropdown">
+                  <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+                  <NavDropdown.Item href="#action4">
+                    Another action
+                  </NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action5">
+                    Something else here
+                  </NavDropdown.Item>
+                </NavDropdown>
+                <Nav.Link href="#" disabled>
+                  Link
+                </Nav.Link>
+              </Nav>
+              <Form className="d-flex">
+                <Form.Control
+                  type="search"
+                  placeholder="Search"
+                  className="me-2"
+                  aria-label="Search"
+                />
+                <Button variant="outline-success">Search</Button>
+              </Form>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      );
 }
+
+export default MainNavbar
