aff4e8f985713443812ff257096978efc68bd724
Jamie Connell <Jamie.Connell_10@hotmail.com>
2024-08-08 19:44:05 +0000

    Navbar.jsx( create handle signout function)

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,3 +5,19 @@ import axios from "axios";
 import { removeTokenTimestamp } from "../pages/utils/Utils";
 import { Navbar, Nav } from "react-bootstrap";
 
+const Navbar = () => {
+
+    const currentUser = useCurrentUser();
+    const setCurrentUser = useSetCurrentUser();
+
+    const handleSignout = async () => {
+        try {
+            await axios.post(`dj-rest-auth/logout/`);
+            removeTokenTimestamp();
+        } catch(err) {
+            consolr.log(err)
+        }
+    }
+
+    
+}
