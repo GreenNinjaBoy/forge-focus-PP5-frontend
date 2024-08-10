9af2e6d1dc705dc0cf7063f2a0b5c53e53612a10
Jamie Connell <Jamie.Connell_10@hotmail.com>
2024-08-10 12:17:25 +0000

    GoalsArea.jsx ( add GoalsArea compnent)

--- src/pages/goals/GoalsArea.jsx
+++ src/pages/goals/GoalsArea.jsx
@@ -1,3 +1,36 @@
 import { useEffect, useState } from "react";
 import { axiosReq } from "../../api/axiosDefaults";
 
+
+export const GoalsArea = ( {id} ) => {
+
+    const [goalsData, setGoalsData] = useState({
+        name:"",
+        reason:"",
+        image:"",
+    });
+
+    const [hasLoaded, setHasLoaded] = useState(false);
+
+    useEffect(()  => {
+        const fetchGoals = async () => {
+            try {
+                const { data } = await axiosReq.get(`/goals`);
+                const { name, reason, image } = data;
+                setGoalsData({ name, reason, image });
+                setHasLoaded(true);
+            } catch (error) {
+                console.log("failed to fetch user goals", error);
+            }
+        };
+
+        fetchGoals();
+    }, [id])
+
+
+  return (
+    <div>
+        {}
+    </div>
+  )
+}
