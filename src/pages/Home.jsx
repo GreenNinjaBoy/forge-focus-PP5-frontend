9639b61052ed6c30a321a25cb5e7cec9817a9d78
Jamie Connell <Jamie.Connell_10@hotmail.com>
2024-08-10 12:07:15 +0000

    pages( goals - create GoalsArea.jsx file)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,3 +3,13 @@ import { Link } from "react-router-dom";
 import { useCurrentUser } from "./contexts/CurrentUserContext";
 import About from "./About";
 
+const Home = () => {
+
+    const currentUser = useCurrentUser();
+
+    const signedOutButtons  = (
+        <>
+        <Link to={'/signup'}>
+        Ready To Forge?</Link></>
+    )
+}
