102ecf52747d94b90c5b62e3befd7fc44478a5e6
Jamie Connell <Jamie.Connell_10@hotmail.com>
2024-08-10 12:55:47 +0000

    GoalsView.jsx( add return statement to file)

--- src/pages/goals/GoalsView.jsx
+++ src/pages/goals/GoalsView.jsx
@@ -9,5 +9,26 @@ const GoalsView = (props) => {
     const handleEdit = () => {
         setGoalState('edit');
     }
-    
+
+    const handleDelete = ()  => {
+        setGoalState('delete');
+    }
+
+    return (
+        <div>
+            <img src={image} alt='goal'/>
+            <h2>{name}</h2>
+            <p>{reason}</p>
+            <div>
+                <button onClick={handleEdit}>
+                    Edit Goal
+                    </button>
+                <button onClick={handleDelete}>
+                    Delete Goal
+                    </button>
+            </div>
+        </div>
+    )
 }
+
+export default GoalsView
