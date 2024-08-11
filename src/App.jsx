a0d6e175bdc23c5a729b8e29d1612fa89984754d
Jamie Connell <Jamie.Connell_10@hotmail.com>
2024-08-11 18:11:23 +0000

    app.jsx(add import statements to file)

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,11 @@
+import { useEffect, useState } from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
+import './api/axiosDefaults';
+import Home from './pages/Home';
+import About from './pages/Home';
+import SignIn from './pages/auth/Signin';
+import SignOut from './pages/auth/SignOut';
+import { useCurrentUser } from './pages/contexts/CurrentUserContext';
 import './App.css';
 import GoalsArea from './pages/goals/GoalsArea';
 import MainNavBar from './components/NavBar';
