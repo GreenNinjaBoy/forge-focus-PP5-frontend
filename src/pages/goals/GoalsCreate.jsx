11d102158103c7901b31a9bc547d7ab223151c63
Jamie Connell <Jamie.Connell_10@hotmail.com>
2024-08-14 20:14:44 +0000

    GoalsCreate.jsx

--- src/pages/goals/GoalsCreate.jsx
+++ src/pages/goals/GoalsCreate.jsx
@@ -6,6 +6,29 @@ import { axiosReq } from "../../api/axiosDefaults";
 
 
 const GoalsCreate = () => {
+
+    const [goalsData, setGoalData] = useState ({
+        name:'',
+        reason:'',
+        image:'',
+    });
+
+    const navigate = useNavigate();
+
+    const handleBack = () => {
+        navigate(-1);
+    }
+
+    const [errors, setErrors] = useState({});
+
+    const {name, reason, image} = goalsData;
+
+    const imageInput = useRef(null);
+
+    
+
+
+
   return (
     <div>GoalsCreate</div>
   )
