fbc815c18ef14860c929b925553ab79f1898492b
Jamie Connell <Jamie.Connell_10@hotmail.com>
2024-08-14 20:05:24 +0000

    GoalsCreate.jsx( add dimport sdtatements to file)

---
+++ src/pages/goals/GoalsCreate.jsx
@@ -0,0 +1,14 @@
+import { useRef, useState } from "react";
+import { Button, Form, Image} from 'react-bootstrap';
+import { Link, useNavigate } from "react-router-dom";
+import { axiosReq } from "../../api/axiosDefaults";
+
+
+
+const GoalsCreate = () => {
+  return (
+    <div>GoalsCreate</div>
+  )
+}
+
+export default GoalsCreate
