df439dae5c8591cec9ef5858f0e8ee6a70345559
Jamie Connell <Jamie.Connell_10@hotmail.com>
2024-08-15 20:27:49 +0000

    GoalsEdit.jsx(add handlechange/changeImage variables)

--- src/pages/goals/GoalsEdit.jsx
+++ src/pages/goals/GoalsEdit.jsx
@@ -13,6 +13,35 @@ const GoalsEdit = (props) => {
         setGoalData,
     } = props;
 
+    const [newData, setNewData] = useState({
+        newName: name;
+        newReason: reason;
+        newImage: image;     
+    });
+    
+    const { newName, newReason, newImage} = newData;
+
+    const [errors, setErrors] = useState({});
+
+    const imageInput = useRef(null);
+
+    const handleChange = (event) => {
+        setNewData({
+            ...newData,
+            [event.target.name]: event.target.value,
+        });
+    };
+
+    const handleChangeImage = (event) => {
+        if (event.target.files.length > 0){
+            URL.revokeObjectURL(image);
+            setNewData({
+                ...newData,
+                newImage: URL.createObjectURL(event.target.files[0])
+            });
+        }
+    };
+
     
   return (
     <div>GoalsEdit</div>
