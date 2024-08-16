2d123077839d241493677136a25d4f02b6a4532b
Jamie Connell <Jamie.Connell_10@hotmail.com>
2024-08-16 21:06:23 +0000

    About.jsx(fill in some details within captions)

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -16,28 +16,31 @@ const About = () => {
         return (
             <div>
                 <div>
-                    <h1>Welcome to Forge Focus</h1>
+                    <h1>Forge Focus</h1>
                 </div>
                 <Carousel activeIndex={index} onSelect={handleSelect} >
                     <Carousel.Item>
                         <img src={ExampleCarouselImage} alt="First slide" />
                         <Carousel.Caption>
-                            <h3>First slide label</h3>
-                            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                            <h3>Welcome</h3>
+                            <p>When you strive to achieve your goals in life but find it difficult to keep things in order then Forge Focus is the place for you!</p>
                         </Carousel.Caption>
                     </Carousel.Item>
                     <Carousel.Item>
                         <img src={ExampleCarouselImage} alt="Second slide" />
                         <Carousel.Caption>
-                            <h3>Second slide label</h3>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                            <h3>About Forge Focus</h3>
+                            <p>Forge Focus provides an area where you can take action and plan.</p>
+                            <p>We aim to provide you the user an easy place to which you can create and refine on areas you either wish to achieve or improve on in your day to day life</p>
+                            <p>From here you can create new goal, add tasks and view goals that you have already been aiming to achieve</p>
                         </Carousel.Caption>
                     </Carousel.Item>
                     <Carousel.Item>
                         <img src={ExampleCarouselImage} alt="Third slide" />
                         <Carousel.Caption>
-                            <h3>Third slide label</h3>
-                            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                            <h3>Creating Goals</h3>
+                            <p>By creating areas of refinement, you the user can define and seperate all the different areas of your life that you wish to focus on and refine.</p>
+                            <p>With each goal, you can then set tasks within that goal to help you achieve</p>
                         </Carousel.Caption>
                     </Carousel.Item>
                 </Carousel>
