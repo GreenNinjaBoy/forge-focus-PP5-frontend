d2b647357f5eb120780c5cdc6d15869dac2062fd
Jamie Connell <Jamie.Connell_10@hotmail.com>
2024-08-23 10:08:51 +0000

    GoalsDesktopHighlight.jsx( install proptypes to solve esLint error)

--- src/pages/goals/GoalsDesktopHighlight.jsx
+++ src/pages/goals/GoalsDesktopHighlight.jsx
@@ -1,5 +1,45 @@
-import {useState, useEffect} from "react";
+// import {useState, useEffect} from "react";
 import { Link } from "react-router-dom";
-import { axiosReq } from "../../api/axiosDefaults";
-import { Spinner } from "react-bootstrap";
+import PropTypes from 'prop-types';
+// import { axiosReq } from "../../api/axiosDefaults";
+// import { Spinner } from "react-bootstrap";
 
+
+const GoalsDesktopHighlight = (props) => {
+    const {
+        name,
+        reason,
+        image,
+        id,
+    } = props;
+
+
+    return (
+        <div>
+            <div>
+                <img src={image} alt={`${name} goal`} />
+                <div>
+                    <h2>{name}</h2>
+                    <p>{reason}</p>
+                </div>
+            </div>
+            <div>            
+            <p>Tasks will go here once created</p>
+            </div>
+            <div>
+                <Link to={`/goals/${id}`}>
+                Click to view Goal
+                </Link>
+            </div>
+        </div>
+    );
+};
+
+GoalsDesktopHighlight.propTypes = {
+    name: PropTypes.string.isRequired,
+    reason: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    id: PropTypes.number.isRequired,
+}
+
+export default GoalsDesktopHighlight
