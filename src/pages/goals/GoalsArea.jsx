1a8cb7102fc97d31de7be8676a6de9ccc85b1a7e
Jamie Connell <Jamie.Connell_10@hotmail.com>
2024-08-23 09:11:03 +0000

    goals( create goalsdesktop jsx file)

--- src/pages/goals/GoalsArea.jsx
+++ src/pages/goals/GoalsArea.jsx
@@ -1,12 +1,16 @@
 import { useEffect, useState } from "react";
 import { axiosReq } from "../../api/axiosDefaults";
+import GoalsView from "./GoalsView";
 import GoalsEdit from "./GoalsEdit";
 import GoalsDelete from "./GoalsDelete";
 import { useNavigate } from "react-router-dom";
+import styles from "../../styles/GoalsView.module.css";
 
 const GoalsArea = ({ id }) => {
 
     const [goalsData, setGoalsData] = useState([]);
+
+    const [goalsState, setGoalsSate] = useState("view");
 
     const [hasLoaded, setHasLoaded] = useState(false);
 
@@ -34,18 +38,29 @@ const GoalsArea = ({ id }) => {
         };
 
         fetchGoals();
-    }, [id]);
+    }, [navigate, id])
+
+    function GoalsContext() {
+        if (goalsState==='view') {
+            return <GoalsView {...goalsData} setGoalsData={setGoalsData} setGoalsState={setGoalsSate} />
+        } else if (goalsState==='edit') {
+            return <GoalsEdit {...goalsData} id={id} setGoalsData={setGoalsData} setGoalsSate={setGoalsSate} />
+        } else if (goalsState==='delete') {
+            return <GoalsDelete {...goalsData} id={id} setGoalsSate={setGoalsSate} />
+        }
+    };
 
     return (
-        <div>
+        <div className={styles.GoalsContainer}>
             {hasLoaded ? (
-                goalsData.map((goal) => (
-                    <div key={goal.id}>
-                        <h1>{goal.name}</h1>
-                        <p>{goal.reason}</p>
-                        <img src={goal.image} alt={goal.name} />
-                    </div>
-                ))
+                <GoalsContext />
+                // goalsData.map((goal) => (
+                //     <div key={goal.id}>
+                //         <h1>{goal.name}</h1>
+                //         <p>{goal.reason}</p>
+                //         <img src={goal.image} alt={goal.name} />
+                    // </div>
+                // ))
             ) : (
                 <p>Loading Goals Data....</p>
             )}
