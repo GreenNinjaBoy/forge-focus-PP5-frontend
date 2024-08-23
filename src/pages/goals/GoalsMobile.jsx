0b014acb855eb936a73269ee52f81a4203627179
Jamie Connell <Jamie.Connell_10@hotmail.com>
2024-08-23 10:23:40 +0000

    GoalsMobile.jsx ( add import statements)

---
+++ src/pages/goals/GoalsMobile.jsx
@@ -0,0 +1,8 @@
+import { useContext, useState } from "react";
+import { Accordion, 
+        Card,
+        UseAccordionToggle,
+        AccordionContext } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import GoalsArea from "./GoalsArea";
+
