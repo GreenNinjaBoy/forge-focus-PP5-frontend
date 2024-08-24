1c95773516546dd95c84428bbd50806d4b941cb5
Jamie Connell <Jamie.Connell_10@hotmail.com>
2024-08-24 20:39:14 +0000

    home.jsx( and structure to homepage)

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Home from './pages/Home';
 import About from './pages/About';
 import SignUp from './pages/auth/Signup';
 import SignIn from './pages/auth/Signin';
-// import GoalsCreate from './pages/goals/GoalsCreate';
+import GoalsCreate from './pages/goals/GoalsCreate';
 import { useCurrentUser } from './pages/contexts/CurrentUserContext';
 import './App.css';
 import MainNavBar from './components/NavBar';
@@ -46,6 +46,7 @@ function App() {
         <Route path="/signup" element={<SignUp />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="goalsarea" element={<GoalsArea />} />
+        <Route path="goalscreate" element={<GoalsCreate />} />
         <Route path="/goals" element={<Goals />} />
         <Route path="home" element={<Home />} />
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,64 @@
 import { useEffect, useState } from "react";
 import { axiosReq } from '../api/axiosDefaults';
+import { useCurrentUser, useSetCurrentUser } from '../pages/contexts/CurrentUserContext';
+import { Button } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
-  
-  return (
-    <div>Welcome to the home page</div>
-  )
-}
+  const currentUser = useCurrentUser();
+  const setCurrentUser = useSetCurrentUser();
 
-export default Home
+  const navigate = useNavigate();
+
+  const [goalsCount, setGoalsCount] = useState(0);
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const userResponse = await axiosReq.get('/user/');
+        setCurrentUser(userResponse.data);
+      } catch (err) {
+        console.error("Failed to fetch user data", err);
+      }
+    };
+
+    fetchUserData();
+  }, [setCurrentUser]);
+
+  useEffect(() => {
+    const fetchGoalsData = async () => {
+      try {
+        const goalsResponse = await axiosReq.get('/goals/');
+        setGoalsCount(goalsResponse.data.results.length);
+      } catch (err) {
+        console.error("Failed to fetch goals data", err);
+      }
+    };
+
+    if (currentUser) {
+      fetchGoalsData();
+    }
+  }, [currentUser]);
+
+  return (
+    <div>
+      { currentUser ? (
+        <>
+        <div>
+        <h1>Welcome {currentUser.username}</h1>
+        <p>You currently have {goalsCount} goals you are working hard to achieve</p>
+        </div>
+        <div>
+          <Button onClick={() => navigate('/goalscreate')}>Create New Goal</Button>
+        </div>
+        </>
+      ) : (
+        <div>
+          <h2>No User information to display</h2>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Home;
