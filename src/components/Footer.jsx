70cfb7d1e585a8b0ce25bd0c2770b9da2e0218f4
Jamie Connell <Jamie.Connell_10@hotmail.com>
2024-08-28 19:03:51 +0000

    Footer.jsx (insert return statement within file)

---
+++ src/components/Footer.jsx
@@ -0,0 +1,32 @@
+
+
+const Footer = () => {
+  return (
+    <footer>
+        <p>
+            <span>Created by</span>
+            Jamie Connell Student of code Institute
+            <span>For Educational Purposes only</span>
+            <span>2024</span>
+        </p>
+        <span>
+                <a href="https://github.com/GreenNinjaBoy"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    aria-label="Visit my GitHub profile (opens in a new tab)"
+                >
+                    <i className="fa-brands fa-github"></i>
+                </a>
+                <a href="https://www.linkedin.com/in/jamie-connell-995748193/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    aria-label="Visit my linkedin profile (opens in a new tab)"
+                >
+                    <i className="fa-brands fa-linkedin"></i>
+                </a>
+            </span>
+    </footer>
+  )
+}
+
+export default Footer
