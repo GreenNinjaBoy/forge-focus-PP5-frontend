bc81f3330bd8d7bb6949bfc10b80817c8ef310a9
Jamie Connell <Jamie.Connell_10@hotmail.com>
2024-09-05 22:07:36 +0000

    tasksArea.jsx (add variable statements)

--- src/pages/tasks/TasksArea.jsx
+++ src/pages/tasks/TasksArea.jsx
@@ -3,7 +3,15 @@ import { useEffect, useState } from "react";
 import { axiosReq } from "../../api/axiosDefaults";
 import { useNavigate } from "react-router-dom";
 
-const TasksArea = () => {
+const TasksArea = ( {id} ) => {
+    const [tasksData, setTasksData] = useState([]);
+    const [activeTasks, setActiveTasks] = useSate([]);
+    const [completedTasks, setCompletedTasks] = useState([]);
+    const [hasLoaded, setHasLoaded] = useState([]);
+    const [searchTerm, setSearchTerm] = useState("");
+    const navigate = useNavigate();
+
+
   return (
     <div>TasksArea</div>
   )
