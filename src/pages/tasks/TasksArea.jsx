f7892c828354aed2bfc0b5218f4fe0d50367787d
Jamie Connell <Jamie.Connell_10@hotmail.com>
2024-09-05 21:58:25 +0000

    TasksArea.jsx( add import statements)

---
+++ src/pages/tasks/TasksArea.jsx
@@ -0,0 +1,12 @@
+import PropTypes from "prop-types";
+import { useEffect, useState } from "react";
+import { axiosReq } from "../../api/axiosDefaults";
+import { useNavigate } from "react-router-dom";
+
+const TasksArea = () => {
+  return (
+    <div>TasksArea</div>
+  )
+}
+
+export default TasksArea
