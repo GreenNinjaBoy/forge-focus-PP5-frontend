33cd9c1988a985c4ce30ecde76c9223457347f44
Jamie Connell <Jamie.Connell_10@hotmail.com>
2024-09-26 21:50:30 +0000

    hauth add comments to files)

--- src/pages/auth/Signin.jsx
+++ src/pages/auth/Signin.jsx
@@ -7,38 +7,60 @@ import { useRedirect } from "../../hooks/useRedirect";
 import { useSetGlobalSuccessMessage, useSetShowGlobalSuccess } from "../../hooks/useGlobalSuccess";
 import styles from '../../styles/Form.module.css';
 
+/**
+ * SignIn component for handling user sign-in.
+ */
 function SignIn() {
+  // Get the function to set the current user from the custom hook
   const setCurrentUser = useSetCurrentUser();
+  
+  // Get the current user from the custom hook
   const currentUser = useCurrentUser();
+  
+  // Redirect authenticated users to the home page
   useRedirect("loggedIn", currentUser);
 
+  // Get the function to show the global success message from the custom hook
   const setShowGlobalSuccess = useSetShowGlobalSuccess();
+  
+  // Get the function to set the global success message from the custom hook
   const setGlobalSuccessMessage = useSetGlobalSuccessMessage();
 
+  // State to manage the login form data
   const [logInData, setLogInData] = useState({
     username: "",
     password: "",
   });
   const { username, password } = logInData;
 
+  // State to manage form validation errors
   const [errors, setErrors] = useState({});
 
+  // Get the navigate function from react-router-dom to programmatically navigate
   const navigate = useNavigate();
 
+  // Function to handle form submission
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
+      // Make a POST request to the login endpoint with the form data
       const { data } = await axios.post("/dj-rest-auth/login/", logInData);
+      // Set the current user with the response data
       setCurrentUser(data.user);
+      // Set the token timestamp for authentication
       setTokenTimestamp(data);
+      // Set and show the global success message
       setGlobalSuccessMessage("You are now signed in.");
       setShowGlobalSuccess(true);
+      // Navigate to the home page
       navigate("/home");
     } catch (err) {
+      // Set validation errors if the request fails
       setErrors(err.response?.data || {});
     }
   };
 
+  // Function to handle form input changes
   const handleChange = (event) => {
     setLogInData({
       ...logInData,
--- src/pages/auth/Signup.jsx
+++ src/pages/auth/Signup.jsx
@@ -4,10 +4,20 @@ import { useNavigate } from 'react-router-dom';
 import { useSetGlobalSuccessMessage, useSetShowGlobalSuccess } from "../../hooks/useGlobalSuccess";
 import styles from '../../styles/Form.module.css';
 
+/**
+ * SignUp component for handling user registration.
+ * Manages form state, handles form submission, and displays validation errors.
+ * Shows a global success message upon successful registration and redirects to the sign-in page.
+ */
+
 const SignUp = () => {
+    // Get the function to show the global success message from the custom hook
     const setShowGlobalSuccess = useSetShowGlobalSuccess();
+    
+    // Get the function to set the global success message from the custom hook
     const setGlobalSuccessMessage = useSetGlobalSuccessMessage();
 
+    // State to manage the signup form data
     const [signupData, setSignUpData] = useState({
         username: '',
         email: '',
@@ -17,10 +27,13 @@ const SignUp = () => {
 
     const { username, email, password1, password2 } = signupData;
 
+    // Get the navigate function from react-router-dom to programmatically navigate
     const navigate = useNavigate();
 
+    // State to manage form validation errors
     const [errors, setErrors] = useState({});
 
+    // Function to handle form input changes
     const handleChange = (event) => {
         setSignUpData({
             ...signupData,
@@ -28,16 +41,21 @@ const SignUp = () => {
         });
     };
 
+    // Function to handle form submission
     const handleSubmit = async (event) => {
         event.preventDefault();
         try {
+            // Make a POST request to the registration endpoint with the form data
             await axios.post('/dj-rest-auth/registration/', signupData);
+            // Set and show the global success message
             setGlobalSuccessMessage("You have Signed up Successfully, you can now sign in!");
             setShowGlobalSuccess(true);
+            // Navigate to the sign-in page
             navigate('/signin');
         } catch (err) {
             console.error("Error response:", err.response);
             if (err.response && err.response.data) {
+                // Set validation errors if the request fails
                 setErrors(err.response.data);
             } else {
                 setErrors({ non_field_errors: ["An unexpected error occurred. Please try again."] });
@@ -47,73 +65,73 @@ const SignUp = () => {
 
     return (
         <div className={styles.authContainer}>
-          <div className={styles.contentWrapper}>
-            <h1 className={styles.heading}>Sign Up</h1>
-            <div className={styles.formCard}>
-                <form onSubmit={handleSubmit}>
-                    <div className={styles.formRow}>
-                        <div className={styles.formColumn}>
-                            <div className={styles.formGroup}>
-                                <label htmlFor="username" className={styles.formLabel}>Username:</label>
-                                <input
-                                    type="text"
-                                    id="username"
-                                    name="username"
-                                    value={username}
-                                    onChange={handleChange}
-                                    className={styles.formControl}
-                                />
+            <div className={styles.contentWrapper}>
+                <h1 className={styles.heading}>Sign Up</h1>
+                <div className={styles.formCard}>
+                    <form onSubmit={handleSubmit}>
+                        <div className={styles.formRow}>
+                            <div className={styles.formColumn}>
+                                <div className={styles.formGroup}>
+                                    <label htmlFor="username" className={styles.formLabel}>Username:</label>
+                                    <input
+                                        type="text"
+                                        id="username"
+                                        name="username"
+                                        value={username}
+                                        onChange={handleChange}
+                                        className={styles.formControl}
+                                    />
+                                </div>
+                                <div className={styles.formGroup}>
+                                    <label htmlFor="password1" className={styles.formLabel}>Password:</label>
+                                    <input
+                                        type="password"
+                                        id="password1"
+                                        name="password1"
+                                        value={password1}
+                                        onChange={handleChange}
+                                        className={styles.formControl}
+                                    />
+                                </div>
                             </div>
-                            <div className={styles.formGroup}>
-                                <label htmlFor="password1" className={styles.formLabel}>Password:</label>
-                                <input
-                                    type="password"
-                                    id="password1"
-                                    name="password1"
-                                    value={password1}
-                                    onChange={handleChange}
-                                    className={styles.formControl}
-                                />
+                            <div className={styles.formColumn}>
+                                <div className={styles.formGroup}>
+                                    <label htmlFor="email" className={styles.formLabel}>Email:</label>
+                                    <input
+                                        type="email"
+                                        id="email"
+                                        name="email"
+                                        value={email}
+                                        onChange={handleChange}
+                                        className={styles.formControl}
+                                    />
+                                </div>
+                                <div className={styles.formGroup}>
+                                    <label htmlFor="password2" className={styles.formLabel}>Confirm Password:</label>
+                                    <input
+                                        type="password"
+                                        id="password2"
+                                        name="password2"
+                                        value={password2}
+                                        onChange={handleChange}
+                                        className={styles.formControl}
+                                    />
+                                </div>
                             </div>
                         </div>
-                        <div className={styles.formColumn}>
-                            <div className={styles.formGroup}>
-                                <label htmlFor="email" className={styles.formLabel}>Email:</label>
-                                <input
-                                    type="email"
-                                    id="email"
-                                    name="email"
-                                    value={email}
-                                    onChange={handleChange}
-                                    className={styles.formControl}
-                                />
-                            </div>
-                            <div className={styles.formGroup}>
-                                <label htmlFor="password2" className={styles.formLabel}>Confirm Password:</label>
-                                <input
-                                    type="password"
-                                    id="password2"
-                                    name="password2"
-                                    value={password2}
-                                    onChange={handleChange}
-                                    className={styles.formControl}
-                                />
-                            </div>
-                        </div>
-                    </div>
-                    {Object.keys(errors).map((key) => (
-                        errors[key].map((message, idx) => (
-                            <div key={`${key}-${idx}`} className={styles.alert}>
-                                {message}
-                            </div>
-                        ))
-                    ))}
-                    <button type="submit" className={styles.submitButton}>
-                        Sign Up
-                    </button>
-                </form>
+                        {Object.keys(errors).map((key) => (
+                            errors[key].map((message, idx) => (
+                                <div key={`${key}-${idx}`} className={styles.alert}>
+                                    {message}
+                                </div>
+                            ))
+                        ))}
+                        <button type="submit" className={styles.submitButton}>
+                            Sign Up
+                        </button>
+                    </form>
+                </div>
             </div>
-          </div>
         </div>
     );
 };
