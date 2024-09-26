762429d431732ce03f6e59078df92e68753ec2dc
Jamie Connell <Jamie.Connell_10@hotmail.com>
2024-09-26 21:33:10 +0000

    Footer.jsx ( add comment with brief description)

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,34 +1,44 @@
 import styles from '../styles/Footer.module.css';
 
+
+/**
+ * Footer component for the application.
+ * This component shows my name and states that the application is for educational purposes only.
+ * This component also includes links to GitHub and LinkedIn profiles with icons.
+ * Some text and links are styled to be visible only on desktop devices.
+ */
+
 const Footer = () => {
-    return (
-        <footer className={styles.Footer}>
-            <p>
-                <span className={styles.Desktoponly}>Created by </span>
-                Jamie Connell Student of code Institute
-                <span className={styles.DesktopOnly}>For Educational Purposes only</span>
-                <span className={styles.DesktopOnly}>2024</span>
-            </p>
-            <span>
-                <a href="https://github.com/GreenNinjaBoy"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    aria-label="Visit my GitHub profile (opens in a new tab)"
-                    className={styles.DesktopOnly}
-                >
-                    <i className="fa-brands fa-github"></i>
-                </a>
-                <a href="https://www.linkedin.com/in/jamie-connell-995748193/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    aria-label="Visit my linkedin profile (opens in a new tab)"
-                    className={styles.DesktopOnly}
-                >
-                    <i className="fa-brands fa-linkedin"></i>
-                </a>
-            </span>
-        </footer>
-    );
+  return (
+    <footer className={styles.Footer}> 
+      <p>
+        <span className={styles.Desktoponly}>Created by </span> 
+        Jamie Connell Student of Code Institute 
+        <span className={styles.DesktopOnly}>For Educational Purposes only</span> 
+        <span className={styles.DesktopOnly}>2024</span> 
+      </p>
+      <span>
+        <a
+          href="https://github.com/GreenNinjaBoy"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="Visit my GitHub profile (opens in a new tab)"
+          className={styles.DesktopOnly}
+        >
+          <i className="fa-brands fa-github"></i> 
+        </a>
+        <a
+          href="https://www.linkedin.com/in/jamie-connell-995748193/"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="Visit my LinkedIn profile (opens in a new tab)"
+          className={styles.DesktopOnly}
+        >
+          <i className="fa-brands fa-linkedin"></i>
+        </a>
+      </span>
+    </footer>
+  );
 }
 
-export default Footer;
+export default Footer; 
