0306789037d76ca91b26b3cfcfcd6c0107e74966
Jamie Connell <Jamie.Connell_10@hotmail.com>
2024-09-26 21:52:01 +0000

    Contact.jsx(add comments to file)

--- src/pages/contact/ContactForm.jsx
+++ src/pages/contact/ContactForm.jsx
@@ -4,10 +4,20 @@ import axios from "axios";
 import { useSetGlobalSuccessMessage, useSetShowGlobalSuccess } from '../../hooks/useGlobalSuccess';
 import styles from '../../styles/Form.module.css';
 
+/**
+ * ContactForm component for handling user contact submissions.
+ * Manages form state, handles form submission, and displays validation errors.
+ * Shows a global success message upon successful submission and redirects to the home page.
+ */
+
 const ContactForm = () => {
+    // Get the function to show the global success message from the custom hook
     const setShowGlobalSuccess = useSetShowGlobalSuccess();
+    
+    // Get the function to set the global success message from the custom hook
     const setGlobalSuccessMessage = useSetGlobalSuccessMessage();
     
+    // State to manage the contact form data
     const [contactData, setContactData] = useState({
         name: "",
         email: "",
@@ -16,22 +26,30 @@ const ContactForm = () => {
 
     const { name, email, message } = contactData;
 
+    // State to manage form validation errors
     const [errors, setErrors] = useState({});
 
+    // Get the navigate function from react-router-dom to programmatically navigate
     const navigate = useNavigate();
 
+    // Function to handle form submission
     const handleSubmit = async (event) => {
         event.preventDefault();
         try {
+            // Make a POST request to the contact endpoint with the form data
             await axios.post("/contact/", contactData);
+            // Set and show the global success message
             setGlobalSuccessMessage("Thank you for contacting Forge Focus");
             setShowGlobalSuccess(true);
+            // Navigate to the home page
             navigate("/");
         } catch (err) {
+            // Set validation errors if the request fails
             setErrors(err.response?.data);
         }
     };
     
+    // Function to handle form input changes
     const handleChange = (event) => {
         setContactData({
             ...contactData,
